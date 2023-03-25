011660657f34f88080619fadb3be34d2c573c11d
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-03-25 13:20:41 -0600

    Complete the library iteration

--- src/library.js
+++ src/library.js
@@ -13,17 +13,6 @@ function addBook(library, book){
   library.shelves[book.genre].push(book)
 }
 
-// function checkoutBook(library, title, shelf){
-
-//   for (var i = 0; i < shelf.length; i++) {
-//     if (shelf[i].title === title){
-//       return shelf.splice(i, 1) + `You have now checked out ${title} from the ${library}.`
-//     }
-//    }
-// }
-
-
-
 function checkoutBook(library, title, genre){
   shelf = library.shelves[genre]
   var titleExists = false
@@ -40,17 +29,11 @@ function checkoutBook(library, title, genre){
 }
 }
 
-// function takeStock(library, genre){
-// return `There are a total of ${library.shelves[genre].length} ${genre} books at the ${library.name}.`
-// }
-
 function takeStock(library, genre){
  if (genre){
   return `There are a total of ${library.shelves[genre].length} ${genre} books at the ${library.name}.`
  } else {
-  return `There are a total of ${library.shelves} books at the ${library.name}.`
-
-  /// try to make fucntion that adds all of the arrays elements from every genre together
+  return `There are a total of ${Object.values(library.shelves).length} books at the ${library.name}.`
  }
   }
 
